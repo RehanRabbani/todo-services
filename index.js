ca8e38d2116e9d40a907a2d6d29f56d2d92bfede
RehanRabbani <rabbanirehan6@gmail.com>
2022-06-28 18:48:56 +0500

    [NXL-002] add post end point

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const port = process.env.PORT || 3000;
 
 const todos = [
   {
-    id: "1",
+    id: 1,
     name: "todo 1",
   },
   {
-    id: "2",
+    id: 2,
     name: "todo 2",
   },
   {
-    id: "3",
+    id: 3,
     name: "todo 3",
   },
 ];
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello world 1");
 });
@@ -27,8 +28,17 @@ app.get("/api/todos", (req, res) => {
 });
 
 app.get("/api/todos/:id", (req, res) => {
-  const todo = todos.find((td) => td.id === req.params.id);
+  const todo = todos.find((td) => td.id === parseInt(req.params.id));
   if (!todo) res.status(404).send("Not found");
+  res.send(todo);
+});
+
+app.post("/api/todos", (req, res) => {
+  const todo = {
+    id: todos.length + 1,
+    name: req.body.name,
+  };
+  todos.push(todo);
   res.send(todo);
 });
 
